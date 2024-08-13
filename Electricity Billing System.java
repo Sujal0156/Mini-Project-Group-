13477913db1431c7d7b4a0acd097d5349e4e68c1
Sujal0156 <sujalrupani23@gmail.com>
2024-08-13 20:18:06 +0530

    Update Electricity Billing System.java

--- Electricity Billing System.java
+++ Electricity Billing System.java
@@ -1,1 +1,24 @@
+public class Customer {
+    private String name;
+    private int customerId;
+    private String address;
+
+    public Customer(String name, int customerId, String address) {
+        this.name = name;
+        this.customerId = customerId;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
 
