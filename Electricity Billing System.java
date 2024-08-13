1d5ba826a69e84576dae126575b5bd6f360cc14f
Sujal0156 <sujalrupani23@gmail.com>
2024-08-13 20:17:10 +0530

    Update Electricity Billing System.java

--- Electricity Billing System.java
+++ Electricity Billing System.java
@@ -1,1 +1,25 @@
+public class ElectricityBill {
+    private Customer customer;
+    private int unitsConsumed;
+    private double ratePerUnit;
+
+    public ElectricityBill(Customer customer, int unitsConsumed, double ratePerUnit) {
+        this.customer = customer;
+        this.unitsConsumed = unitsConsumed;
+        this.ratePerUnit = ratePerUnit;
+    }
+
+    public double calculateBill() {
+        return unitsConsumed * ratePerUnit;
+    }
+
+    public void printBill() {
+        System.out.println("Customer Name: " + customer.getName());
+        System.out.println("Customer ID: " + customer.getCustomerId());
+        System.out.println("Address: " + customer.getAddress());
+        System.out.println("Units Consumed: " + unitsConsumed);
+        System.out.println("Rate per Unit: " + ratePerUnit);
+        System.out.println("Total Bill: " + calculateBill());
+    }
+}
 
